0d84d6ee5c296d794a6a8bdb813fe1ecfcbf487d
pooja more <poojamore46406@gmail.com>
2026-02-11 23:02:06 +0530

    UpdateRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Login from "./pages/Login";
-import CompanyDashboardEntry from "./dashboards/company/CompanyDashboardEntry";
+import CompanyDashboard from "./pages/company/CompanyDashboard";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/company/dashboard-new" element={<CompanyDashboardEntry />} />
+          <Route path="/company/dashboard-new" element={<CompanyDashboard />} />
           <Route path="*" element={<Login />} />
         </Routes>
       </BrowserRouter>
